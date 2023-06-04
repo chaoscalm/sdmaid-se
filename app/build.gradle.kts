2c4c352d344e36fd1d4bcaf2f57772827866cfae
darken <darken@darken.eu>
2023-06-04 16:36:52 +0200

    FlowShell

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -169,6 +169,7 @@ dependencies {
     implementation(project(":app-common-shizuku"))
     implementation(project(":app-common-io"))
     implementation(project(":app-common-pkgs"))
+    implementation(project(":app-common-shell"))
 
     addDI()
     addCoroutines()
